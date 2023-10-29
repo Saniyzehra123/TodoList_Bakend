cd1c363100d21598691ad034d580d8c171e19ab6
Saniya Zehra <zsaniya973@gmail.com>
2023-10-29 22:15:53 +0530

    initil

---
+++ src/configs/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const connectDB = async ()=>{
+ 
+    try {
+      mongoose.set("strictQuery", false);
+        const conn = await mongoose.connect(process.env.MONOGODB_URL)
+
+     //   console.log(`MongoDB connected : ${conn.connection.host}`.cyan.underline)
+    }
+    catch(error){
+      console.log(error);
+      process.exit(1)
+    }
+}
+
+module.exports= connectDB
